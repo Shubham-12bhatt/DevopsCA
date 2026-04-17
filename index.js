44355f8ab29ac5b1546bb79a572814d612ff3695
Shubham-12bhatt <bhattshubham466@gmail.com>
2026-04-17 15:58:02 +0530

    minor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/health', (req, res) => {
-  res.json({ status: "healthy" });
+  res.json({ status: "ok" });
 });
 
 const PORT = 3000;
